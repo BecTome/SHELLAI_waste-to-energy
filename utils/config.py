2495dfe618181f2337202cc19d790e9eaca02c88
BecTome <alberto.becerra.tome@gmail.com>
2023-07-24 02:15:55 +0200

    Constraints

---
+++ utils/config.py
@@ -0,0 +1,5 @@
+CAP_DEPOT = 20_000 # Capacity of a depot
+CAP_REF = 100_000 # Capacity of a refinery
+N_DEPOTS = 25 # Number of depots
+N_REF = 5 # Number of refineries
+MIN_PROC_BIO_RATE = 0.8 # Minimum processing rate of biomass
